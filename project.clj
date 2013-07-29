1a3aeeec3da6577b144bb48e30a418672fa1e54b
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-07-29 18:08:25 -0400

    bumped up Selmer version

--- project.clj
+++ project.clj
@@ -12,6 +12,6 @@
                  [stencil "0.3.2"]
                  [selmer "0.1.8"]
                  [tinsel "0.4.0" :exclusions [hickory]]
-                 [selmer "0.1.8"]
+                 [selmer "0.2.1"]
                  [me.raynes/laser "1.1.1"]
                  [me.shenfeng/mustache "1.1"]])
